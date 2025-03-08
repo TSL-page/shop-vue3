1d022762832c6dbb81f9a4957f71af3d14bfdfb2
TSL-page <2962380651@qq.com>
2025-03-08 17:47:57 +0800

    wanjie

---
+++ src/apis/order.js
@@ -0,0 +1,16 @@
+import httpInstance from "@/utils/http";
+
+/* 获取我的订单数据
+params: {
+  orderState:0,
+  page:1,
+  pageSize:2
+}
+*/
+export const getUserOrderAPI = (params) => {
+  return httpInstance({
+    url: '/member/order',
+    method: 'GET',
+    params
+  })
+}
